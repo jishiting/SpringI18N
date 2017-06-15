aa1226bfdf69ba7ecb0ea2c9cf3da163f3f5a52a
姬世霆_Jacky <jishiting@users.noreply.github.com>
2017-06-15 10:54:41 +0300

    add three functions that will blind the value with annotation

--- src/main/java/com/demo/controller/ThemeController.java
+++ src/main/java/com/demo/controller/ThemeController.java
@@ -7,6 +7,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.ThemeResolver;
@@ -27,5 +28,26 @@ public class ThemeController {
 		logger.info("current theme change to " + themeResolver.resolveThemeName(request));
 		return new ModelAndView("redirect:/");
 	}
-
+	
+	//{themeName} must be same with the parameter named by themeName
+	//if they are not same, use @PathVariable("themeName") before your parameter 
+	@RequestMapping("/changeThemeByURL/{themeName}")
+	public ModelAndView changeThemeByURL(HttpServletRequest request, HttpServletResponse response,@PathVariable("themeName") String themeName) {
+		//http://localhost:8081/i18n/theme/changeThemeByURL/theme_arabic
+		logger.info("current theme is " + themeResolver.resolveThemeName(request));
+		themeResolver.setThemeName(request, response, themeName);
+		logger.info("current theme change to " + themeResolver.resolveThemeName(request));
+		return new ModelAndView("redirect:/");
+	}
+	
+	@RequestMapping("/changeThemeParamGetFromURL")
+	public ModelAndView changeThemeParamGetFromURL(HttpServletRequest request, HttpServletResponse response, String themeName) {
+		//http://localhost:8081/i18n/theme/changeThemeParamGetFromURL?themeName1=theme_arabic
+		String themeName1 = request.getParameter("themeName1");
+		System.out.println(themeName1);
+		logger.info("current theme is " + themeResolver.resolveThemeName(request));
+		themeResolver.setThemeName(request, response, themeName);
+		logger.info("current theme change to " + themeResolver.resolveThemeName(request));
+		return new ModelAndView("redirect:/");
+	}
 }
